2701ab86a4b0928c940eca532f870b3fa7fa37a5
Khizar Haider <shaid029@uottawa.ca>
2026-01-22 17:48:34 -0500

    Added more seasons

--- data_loader.py
+++ data_loader.py
@@ -8,11 +8,16 @@ from metrics import prepare_team_games_for_metrics
 
 # Available seasons (hardcoded - NBA API supports these)
 AVAILABLE_SEASONS = [
+    "2025-26",
     "2024-25",
     "2023-24",
     "2022-23",
     "2021-22",
     "2020-21",
+    "2019-20",
+    "2018-19",
+    "2017-18",
+    "2016-17",
 ]
 
 
